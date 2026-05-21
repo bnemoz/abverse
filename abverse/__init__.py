4b8f5f355d53ef043b85636c5758983c47e920f9
bnemoz <bnemoz@gmail.com>
2026-05-20 22:06:48 -0700

    Bump version to 0.1.3, add test_csv.py to CI

--- abverse/__init__.py
+++ abverse/__init__.py
@@ -4,5 +4,5 @@ from ._csv import parse_csv
 from ._germline_db import build_germline_aa_db
 from ._pipeline import reverse_translate
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __all__ = ["reverse_translate", "build_germline_aa_db", "parse_csv"]
